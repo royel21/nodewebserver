935cb0d7026866f7010a99cedab7d3fbf64c4cfd
Royel Consoro <royelconsoro@hotmail.com>
2019-02-16 20:23:54 -0400

    inital commit

--- models/users.js
+++ models/users.js
@@ -1,17 +1,17 @@
 
 var users = [
-    {Id: 1, UserId: "Rconsoro", UserName: "Royel Consoro"},
-    {Id: 2, UserId: "Rconsoro", UserName: "Royel Consoro"},
-    {Id: 3, UserId: "Rconsoro", UserName: "Royel Consoro"},
-    {Id: 4, UserId: "Rconsoro", UserName: "Royel Consoro"},
-    {Id: 5, UserId: "Rconsoro", UserName: "Royel Consoro"},
-    {Id: 6, UserId: "Rconsoro", UserName: "Royel Consoro"},
-    {Id: 7, UserId: "Rconsoro", UserName: "Royel Consoro"},
-    {Id: 8, UserId: "Rconsoro", UserName: "Royel Consoro"},
-    {Id: 9, UserId: "Rconsoro", UserName: "Royel Consoro"},
-    {Id: 10, UserId: "Rconsoro", UserName: "Royel Consoro"},
-    {Id: 11, UserId: "Rconsoro", UserName: "Royel Consoro"},
-    {Id: 12, UserId: "Rconsoro", UserName: "Royel Consoro"}
+    {Id: 1, UserId: "RC00#", UserName: "Rael Combo 1"},
+    {Id: 2, UserId: "RC002", UserName: "Rael Combo 2"},
+    {Id: 3, UserId: "RC003", UserName: "Rael Combo 3"},
+    {Id: 4, UserId: "RC004", UserName: "Rael Combo 4"},
+    {Id: 5, UserId: "RC005", UserName: "Rael Combo 5"},
+    {Id: 6, UserId: "RC006", UserName: "Rael Combo 6"},
+    {Id: 7, UserId: "RC007", UserName: "Rael Combo 7"},
+    {Id: 8, UserId: "RC008", UserName: "Rael Combo 8"},
+    {Id: 9, UserId: "RC009", UserName: "Rael Combo 9"},
+    {Id: 10, UserId: "RC010", UserName: "Rael Combo 10"},
+    {Id: 11, UserId: "RC011", UserName: "Rael Combo 11"},
+    {Id: 12, UserId: "RC012", UserName: "Rael Combo 12"}
   ]
 
 module.exports = users;
