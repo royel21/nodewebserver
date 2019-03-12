2c87c2b9c04e2abfa8a51d7879a337808c991a7e
royel21 <royelconsoro@hotmail.com>
2019-03-12 06:15:28 -0400

    testing

--- testing/test.js
+++ testing/test.js
@@ -2,13 +2,14 @@ const db = require('../models');
 
 
 
-const loadVideos = async (caId) => {
+const loadVideos = async (caId, A) => {
     let val = "";
     let begin = 0;
-    let itemsPerPage = 5;
+    let itemsPerPage = 18;
 
     let cat = await db.category.findOne({ where: { Id: caId } });
-    if (caId) {
+    let videos = {count: 0, rows: []};
+    if (A) {
         console.time('s')
         let vis = await cat.getVideos();
         let visId = vis.map(c => c.Id);
@@ -27,8 +28,8 @@ const loadVideos = async (caId) => {
             offset: begin,
             limit: itemsPerPage,
         });
-
         console.timeEnd('s')
+        console.log("All",videos.rows.length)
     }
     else {
         let vis = await cat.getVideos({
@@ -42,13 +43,14 @@ const loadVideos = async (caId) => {
         });
         videos.rows = vis.slice(begin, itemsPerPage)
         videos.count = vis.length;
+        console.log("cat V:",videos.rows.length)
     }
 }
 
 db.init().then(() => {
     // console.log("\n\n\n\n\n\n\n\n");
-    // loadVideos("6z1sg").catch(err => {
-    //     console.log(err)
-    // })
+    loadVideos('cqrz1').catch(err => {
+        console.log(err)
+    })
    
 });
