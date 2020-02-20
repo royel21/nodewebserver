b2f1b0e9765b7c18fe1236b790f3f67dd3b65050
royel21 <royelconsoro@hotmail.com>
2020-02-20 18:11:17 -0400

    little changes

--- public/static/js/home/login.js
+++ public/static/js/home/login.js
@@ -2,7 +2,6 @@ window.onload = (e) => {
     document.querySelector('#screenw').value = window.screen.width;
     document.querySelector('#screenh').value = document.body.offsetHeight;
     window.history.pushState({}, "Log In", "/login");
-    window.history.pushState({}, "Log In", "/login");
     window.onpopstate = function(e) {
         history.go(-(history.length - 2));
     }
