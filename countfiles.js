58b4defb1ee6dea07366f76428cb463a454ec371
royel21 <royelconsoro@hotmail.com>
2020-02-20 18:07:00 -0400

    little chanes

--- countfiles.js
+++ countfiles.js
@@ -57,35 +57,109 @@
 //         include: [{
 //             model: db.recent,
 //             where: {
-//                 Id: "rsh0o"
-//             },
-//             order: [
-//                 [db.sqlze.literal("Recents.RecentFiles.LastRead"), 'DESC']
-//             ]
-//         }],
-//         offset: 0,
-//         limit: 27,
-//         where: {
-//             [db.Op.and]: [{
-//                 Name: {
-//                     [db.Op.like]: "%" + "" + "%"
-//                 }
-//             }]
-//         }
-//     });
-//     console.timeEnd("s");
-//     return files;
-// }
+//                 Id: "rsh0o"// // const fs = require("fs-extra")
+// // console.log("Videos:",fs.readdirSync("./static/covers/videos/folder-2vdgi").length)
+// // console.log("Series:",fs.readdirSync("./static/covers/series").length)
 
-// query().then((result) => {
-//     console.log(result.count, result.rows[0]);
-// });
-const moment = require('moment')
-const winex = require('win-explorer');
-const files = winex.ListFilesRO('E:\\Temp\\Hmangas');
+// function titleCase(str) {
+//     var splitStr = str.toLowerCase().split(' ');
+//     for (var i = 0; i < splitStr.length; i++) {
+//         // You do not need to check if i is larger than splitStr length, as your for does that for you
+//         // Assign it back to the array
+//         //splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+//         let temp = [];
+//         let count = 0;
+//         for (let l of splitStr[i].split('')) {
+//             if (count == 0 && /[a-zA-Z]/ig.test(l)) {
+//                 count++;
+//                 temp.push(l.toUpperCase());
+//                 continue;
+//             }
+//             temp.push(l);
+//         }
+//         splitStr[i] = temp.join('');
+//     }
+//     // Directly return the joined string
+//     return splitStr.join(' ');
+// }
+// console.time('capitalize');
+// let str = titleCase("[Tachibana Nagon] Anaume (COMIC Kairakuten BEAST 2019-12) [Chinese] [得民調得痔瘡得漢化] [Digital]");
+// console.timeEnd('capitalize');
+// // console.log(str)
+// let str = "[Tachibana Nagon] Anaume (COMIC Kairakuten BEAST 2019-12) [Chinese] [得民調得痔瘡得漢化] [Digital]....";
+// console.time('capitalize');
+// while (true) {
+//     if (str[str.length - 1] == '.') {
+//         str = str.substr(0, str.length - 2);
+//     } else {
+//         break;
+//     }
+// // }
+// console.timeEnd('capitalize');
+// console.log(str);
+
+// fv 0w1sk
+// rc pjwhn
+// loli - cat - bzy7w
 const db = require('./models/index');
-const fs = require('fs-extra')
-const path = require('path')
+db.sqlze.options.logging = true;
+const helper = require('./controllers/home/file-helper2');
+
+var query = async () => {
+
+
+
+    console.time("s");
+    // let user = await db.user.findOne({
+    //     where: {
+    //         Name: 'Royel'
+    //     }, include: [
+    //         { model: db.userConfig },
+    //         { model: db.favorite },
+    //         { model: db.recent }
+    //     ]
+    // });
+    // let cat = await db.category.findOne();
+    // console.log('\n\n\n');
+    let models = [
+        { model: db.recent, where: { Id: "7z4l9" } },
+        { model: db.favorite }
+    ]
+    let files = await helper.getFiles({ begin: 0, itemsPerPage: 500, search: "" }, models, "recent")
+    // let files = await db.file.findAndCountAll({
+    //     order: [[db.sqlze.literal("LastRead"), 'DESC']],
+    //     attributes:{
+    //         include:[
+    //             [db.sqlze.literal("REPLACE(File.Name, '[','0')"), 'N'],
+    //             [db.sqlze.literal("`Recents->RecentFiles`.`LastRead`"), 'LastRead'],
+    //             [db.sqlze.literal("`Recents->RecentFiles`.`LastPos`"), 'CurrentPos'],
+    //             [db.sqlze.literal("`Favorites`.`UserId`"), 'isFav']
+    //         ]
+    //     },
+    //     include: [
+    //         {
+    //             model:db.category,
+    //             where: {Name: "Flask"}
+    //         },
+    //         { model: db.recent },
+    //         { model: db.favorite}
+    //     ]
+    // });
+
+    console.log("Duol", files.rows[0]);
+    console.timeEnd("s");
+
+}
+
+query().then((result) => {
+
+});
+// const moment = require('moment')
+// const winex = require('win-explorer');
+// const files = winex.ListFilesRO('E:\\Temp\\Hmangas');
+// const db = require('./models/index');
+// const fs = require('fs-extra')
+// const path = require('path')
 
 // const updateDate = async(tfs) => {
 
@@ -108,4 +182,58 @@ const path = require('path')
 
 // updateDate(files);
 
-console.log(winex.ListFiles("H:\\", [], { hidden: true, file: false, directory: true }));
+// console.log(winex.ListFiles("H:\\", [], { hidden: true, file: false, directory: true }));
+//             },
+//             order: [
+//                 [db.sqlze.literal("Recents.RecentFiles.LastRead"), 'DESC']
+//             ]
+//         }],
+//         offset: 0,
+//         limit: 27,
+//         where: {
+//             [db.Op.and]: [{
+//                 Name: {
+//                     [db.Op.like]: "%" + "" + "%"
+//                 }
+//             }]
+//         }
+//     });
+//     console.timeEnd("s");
+//     return files;
+// }
+
+// query().then((result) => {
+//     console.log(result.count, result.rows[0]);
+// });
+// const moment = require('moment')
+// const winex = require('win-explorer');
+// const files = winex.ListFilesRO('E:\\Temp\\Hmangas');
+// const db = require('./models/index');
+// const fs = require('fs-extra')
+// const path = require('path')
+
+// const updateDate = async(tfs) => {
+
+//     console.log(cat)
+// }
+// updateDate();
+
+//     for (let f of tfs) {
+//         if (!f.isDirectory) {
+//             // console.log(new Date(f.LastModified))
+//             let found = await db.file.findOne({ where: { Name: f.FileName } });
+//             if (found) {
+//                 // await found.update({ CreatedAt: f.LastModified });
+//                 // console.log(found.CreatedAt, fs.statSync(path.join(found.FullPath, f.FileName)));
+//                 // console.log(f.LastModified, moment(f.LastModified).toDate(), f.FileName)
+//             }
+//         } else {
+//             updateDate(f.Files);
+//         }
+//     }
+
+// }
+
+// updateDate(files);
+
+// console.log(winex.ListFiles("H:\\", [], { hidden: true, file: false, directory: true }));
--- sqlnotes.sql
+++ sqlnotes.sql
@@ -1,84 +1,55 @@
--- SQLite
--- SELECT Name, REPLACE(Name, '[', '0') as N from Files  where FullPath = 'E:\Temp\Hmangas\yaoi' ORDER by N;
---SELECT `Id`, `Name`, `CoverPath`, REPLACE(`Name`, '[', '0') AS `N` FROM `Series` AS `Serie` WHERE `Serie`.`Name` LIKE '%%' ORDER BY `N` LIMIT 0, 27;
---SELECT Name, COUNT(*) FROM files GROUP BY Name HAVING count(*) > 1;
--- SELECT count(*) AS `count` FROM `Files` AS `File` WHERE (`File`.`Name` LIKE '%%' AND File.Id IN (Select FileId from FileCategories where CategoryId = 'nm4pe'));
--- SELECT `Id`, `Name`, `CurrentPos`, `Duration`, `FullPath`, `Type`, `Size`, `CreatedAt`, `DirectoryId`, `FolderId` FROM `Files` AS `File` WHERE (`File`.`Name` LIKE '%%' AND File.Id  IN (Select FileId from
--- FileCategories where CategoryId = 'nm4pe')) LIMIT 0, 19
--- SELECT count(*) AS `count` FROM `Files` AS `File` WHERE (`File`.`Name` LIKE '%%' AND File.Id  IN (Select FileId from FileCategories where CategoryId = 'nm4pe'));
--- Select count(*) as count from Files where Name LIKE '' and Id in(Select FileId from FileCategories where CategoryId = 'nm4pe');
--- Select Id, Name, NameNormalize
---         from Files where Name LIKE '' and Id  in(Select FileId
---         from FileCategories where CategoryId = 'nm4pe')
---         ORDER BY NameNormalize limit 0, 19;
--- SQLite
---DELETE FROM FavoriteFiles where Id > 0;
----DELETE FROM sqlite_sequence WHERE name='FavoriteFiles';
---SELECT (Select FileId from FavoriteFiles where FileId == files.Id) as N, Id, Name, REPLACE(Name, '[', '0') as H  from files ORDER BY H;
---SELECT `Id`, `Name`, `CurrentPos`, `Duration`, `FullPath`, `Type`, `Size`, `CreatedAt`, `DirectoryId`, `FolderId`, REPLACE(`Name`, '[', '0') AS `N`, (Select FileId from FavoriteFiles where FileId == `File`.`Id`) AS `isFav` FROM `Files` AS `File` WHERE `File`.`Name` LIKE '%%' AND `File`.`Type` = 'Manga' ORDER BY `N` LIMIT 0, 27;
--- SELECT Name From Files WHERE (SELECT COUNT(*) from files;
--- DELETE from files where Name in (SELECT Name, COUNT(*) c FROM files GROUP BY Name HAVING c > 1);
---delete   from files where Name not in ( select  min(ROW_NUMBER()) from  files group by Name );
--- SELECT Name, ROW_NUMBER() OVER(Name) as n, COUNT(*) c FROM files GROUP BY Name HAVING c > 1;
 SELECT
   `File`.*,
-  `Categories`.`Id` AS `Categories.Id`,
-  `Categories->FileCategory`.`Id` AS `Categories.FileCategory.Id`,
-  `Categories->FileCategory`.`CategoryId` AS `Categories.FileCategory.CategoryId`,
-  `Categories->FileCategory`.`FileId` AS `Categories.FileCategory.FileId`
+  `Recents`.`Id` AS `Recents.Id`,
+  `Recents`.`Name` AS `Recents.Name`,
+  `Recents`.`UserId` AS `Recents.UserId`,
+  `Recents->RecentFiles`.`Id` AS `Recents.RecentFiles.Id`,
+  `Recents->RecentFiles`.`LastRead` AS `Recents.RecentFiles.LastRead`,
+  `Recents->RecentFiles`.`LastPos` AS `Recents.RecentFiles.LastPos`,
+  `Recents->RecentFiles`.`RecentId` AS `Recents.RecentFiles.RecentId`,
+  `Recents->RecentFiles`.`FileId` AS `Recents.RecentFiles.FileId`,
+  `Favorites`.`Id` AS `Favorites.Id`,
+  `Favorites`.`Name` AS `Favorites.Name`,
+  `Favorites`.`UserId` AS `Favorites.UserId`,
+  `Favorites->FavoriteFile`.`Id` AS `Favorites.FavoriteFile.Id`,
+  `Favorites->FavoriteFile`.`FavoriteId` AS `Favorites.FavoriteFile.FavoriteId`,
+  `Favorites->FavoriteFile`.`FileId` AS `Favorites.FavoriteFile.FileId`
 FROM (
     SELECT
       `File`.`Id`,
       `File`.`Name`,
+      `File`.`Duration`,
       `File`.`FullPath`,
       `File`.`Type`,
+      `File`.`Size`,
+      `File`.`CreatedAt`,
       `File`.`DirectoryId`,
-      REPLACE(Name, '[', '0') AS `N`,
-      (
-        Select
-          FileId
-        from FavoriteFiles
-        where
-          FileId == File.Id
-          and FavoriteId == 'b1d0p'
-      ) AS `isFav`,
-      (
-        Select
-          LastPos
-        from RecentFiles
-        where
-          FileId == File.Id
-          and RecentId == 'rsh0o'
-      ) AS `CurrentPos`,
-      (
-        Select
-          LastRead
-        from RecentFiles
-        where
-          FileId == File.Id
-          and RecentId == 'rsh0o'
-      ) AS `LastRead`
+      `File`.`FolderId`,
+      REPLACE(File.Name, '[', '0') AS `N`,
+      `Recents->RecentFiles`.`LastRead` AS `LastRead`,
+      `Recents->RecentFiles`.`LastPos` AS `CurrentPos`,
+      `Favorites`.`UserId` AS `isFav`
     FROM `Files` AS `File`
     WHERE
-      `File`.`Name` LIKE '%%'
+      (`File`.`Name` LIKE '%%')
       AND (
         SELECT
-          `FileCategory`.`Id`
-        FROM `FileCategories` AS `FileCategory`
-        INNER JOIN `Categories` AS `Category` ON `FileCategory`.`CategoryId` = `Category`.`Id`
-          AND `Category`.`Id` = 'tna2g'
+          `RecentFiles`.`Id`
+        FROM `RecentFiles` AS `RecentFiles`
+        INNER JOIN `Recents` AS `Recent` ON `RecentFiles`.`RecentId` = `Recent`.`Id`
+          AND `Recent`.`Id` = '7z4l9'
         WHERE
-          (`File`.`Id` = `FileCategory`.`FileId`)
+          (`File`.`Id` = `RecentFiles`.`FileId`)
         LIMIT
           1
       ) IS NOT NULL
     ORDER BY
-      `N`
+      LastRead DESC
     LIMIT
-      0, 21
+      0, 500
   ) AS `File`
-INNER JOIN `FileCategories` AS `Categories->FileCategory` ON `File`.`Id` = `Categories->FileCategory`.`FileId`
-INNER JOIN `Categories` AS `Categories` ON `Categories`.`Id` = `Categories->FileCategory`.`CategoryId`
-  AND `Categories`.`Id` = 'tna2g'
-ORDER BY
-  `N`;
+INNER JOIN `RecentFiles` AS `Recents->RecentFiles` ON `File`.`Id` = `Recents->RecentFiles`.`FileId`
+INNER JOIN `Recents` AS `Recents` ON `Recents`.`Id` = `Recents->RecentFiles`.`RecentId`
+  AND `Recents`.`Id` = '7z4l9'
+LEFT OUTER JOIN `FavoriteFiles` AS `Favorites->FavoriteFile` ON `File`.`Id` = `Favorites->FavoriteFile`.`FileId`
+LEFT OUTER JOIN `Favorites` AS `Favorites` ON `Favorites`.`Id` = `Favorites->FavoriteFile`.`FavoriteId`;
